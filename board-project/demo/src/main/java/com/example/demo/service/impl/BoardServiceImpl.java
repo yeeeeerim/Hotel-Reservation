998ea48305781e4d5ca42ef17c4fadedb78ac678
chalieK <97022321+chalieK@users.noreply.github.com>
2022-12-29 16:24:31 +0900

    Feat: Update BoardServicelmpl.java by Song
    게시판 저장소에서 데이터 가져오기 기능 하단에 추가하고 올립니다!

--- board-project/demo/src/main/java/com/example/demo/service/impl/BoardServiceImpl.java
+++ board-project/demo/src/main/java/com/example/demo/service/impl/BoardServiceImpl.java
@@ -1,17 +1,19 @@
 package com.example.demo.service.impl;
 
 import com.example.demo.data.request.BoardRequestDTO;
+import com.example.demo.data.request.PageRequestDTO;
 import com.example.demo.data.response.BoardResponseDTO;
 import com.example.demo.domain.Board;
 import com.example.demo.repository.BoardRepository;
 import com.example.demo.service.BoardService;
-import io.swagger.annotations.ExampleProperty;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
-
+import org.springframework.ui.page;
 
 @Service
 @RequiredArgsConstructor
@@ -24,7 +26,6 @@ public class BoardServiceImpl implements BoardService {
     public BoardResponseDTO read(Long id) {
         Board board = boardRepository.findById(id)
                 .orElseThrow(RuntimeException::new);
-
 
         return BoardResponseDTO.builder()
                 .id(board.getId())
@@ -40,20 +41,20 @@ public class BoardServiceImpl implements BoardService {
     }
 
     @Override
-    public boolean updateBoard(BoardRequestDTO boardRequestDTO)
-    {
+    public boolean updateBoard(BoardRequestDTO boardRequestDTO) {
         Board board = boardRepository.findById(boardRequestDTO.getId())
                 .orElseThrow(RuntimeException::new);
 
         board.modifyTitleAndContent(boardRequestDTO.getTitle(), boardRequestDTO.getContent());
 
-        try{
-            log.info("updated Data : {}",board);
+        try {
+            log.info("updated Data : {}", board);
             boardRepository.save(board);
 
-        }catch (Exception e){
+        } catch (Exception e) {
             return false;
-        };
+        }
+        ;
 
         return true;
     }
@@ -61,9 +62,13 @@ public class BoardServiceImpl implements BoardService {
     @Override
     public boolean deleteBoard(Long id) {
         boardRepository.deleteById(id);
+
         return false;
     }
-
-
-    //영진 - 메소드를 통해 데이터를 가져옴
+    @Override
+    public Page<Board> boardlist(int page){
+        return boardRepository.findAll(PageRequest.of(page, 10, Sort.by(Sort.Direction.DESC, "id")));
+    }
 }
+    //Page<Post> findAll(Pageable pageable);
+
